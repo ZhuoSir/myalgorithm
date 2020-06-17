34f57c870b15472352a58ebf20a8389e0453539f
sunny <2623581340@qq.com>
2020-06-17 11:29:57 +0800

    二叉树

--- src/main/java/com/chen/MyBinarySearchTree.java
+++ src/main/java/com/chen/MyBinarySearchTree.java
@@ -1,5 +1,7 @@
 package com.chen;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MyBinarySearchTree {
@@ -117,29 +119,144 @@ public class MyBinarySearchTree {
         return (remove(element, root) != null);
     }
 
+    public void reverse() {
+        reverse(root);
+    }
+
+    public void reverse(MyBinaryNode node) {
+
+        if (node == null)
+            return;
+
+        if (node.leftNode == null && node.rightNode == null)
+            return;
+
+        MyBinaryNode temp = node.leftNode;
+        node.leftNode = node.rightNode;
+        node.rightNode = temp;
+
+        reverse(node.leftNode);
+        reverse(node.rightNode);
+    }
+
+    /**
+     * 中序遍历
+     *
+     * */
+    public List<Integer> inorderRecursion() {
+        return inorderRecursion(root);
+    }
+
+    public List<Integer> inorderRecursion(MyBinaryNode node) {
+        if (node == null)
+            return null;
+
+        List<Integer> left = inorderRecursion(node.leftNode);
+        Integer element = node.element;
+        List<Integer> right = inorderRecursion(node.rightNode);
+
+        List<Integer> ret = new ArrayList<Integer>();
+        if (left != null)
+            ret.addAll(left);
+
+        if (element != null)
+            ret.add(element);
+
+        if (right != null)
+            ret.addAll(right);
+
+        return ret;
+    }
+
+    /**
+     * 前序遍历
+     *
+     * */
+    public List<Integer> preOrderRecursion() {
+        return preOrderRecursion(root);
+    }
+
+    public List<Integer> preOrderRecursion(MyBinaryNode node) {
+        if (node == null)
+            return null;
+
+        Integer element = node.element;
+        List<Integer> left = preOrderRecursion(node.leftNode);
+        List<Integer> right = preOrderRecursion(node.rightNode);
+
+        List<Integer> ret = new ArrayList<Integer>();
+        if (element != null)
+            ret.add(element);
+
+        if (left != null)
+            ret.addAll(left);
+
+        if (right != null)
+            ret.addAll(right);
+
+        return ret;
+    }
+
+
+    public List<Integer> postOrderRecursion() {
+        return postOrderRecursion(root);
+    }
+
+    public List<Integer> postOrderRecursion(MyBinaryNode node) {
+        if (node == null)
+            return null;
+
+        List<Integer> left = postOrderRecursion(node.leftNode);
+        List<Integer> right = postOrderRecursion(node.rightNode);
+        Integer element = node.element;
+
+        List<Integer> ret = new ArrayList<Integer>();
+        if (left != null)
+            ret.addAll(left);
+
+        if (right != null)
+            ret.addAll(right);
+
+        if (element != null)
+            ret.add(element);
+
+        return ret;
+    }
+
 
     public static void main(String[] args) {
 
         MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree();
 
-        Random random = new Random(1000);
-        for (int i = 0; i < 100; i++) {
+        Random random = new Random(1234);
+        for (int i = 0; i < 10; i++) {
             int r = random.nextInt(1000);
             myBinarySearchTree.insert(r);
         }
 
-        int max = myBinarySearchTree.findMax();
-        int min = myBinarySearchTree.findMin();
+//        int max = myBinarySearchTree.findMax();
+//        int min = myBinarySearchTree.findMin();
+//
+//        System.out.println(max);
+//        System.out.println(min);
+//
+//        System.out.println(myBinarySearchTree.nodeCount());
+//        System.out.println(myBinarySearchTree.depth());
+//
+//
+//        System.out.println(myBinarySearchTree.remove(7));
+//        System.out.println(myBinarySearchTree.nodeCount());
 
-        System.out.println(max);
-        System.out.println(min);
+//        System.out.println(myBinarySearchTree.inorderRecursion());
+//        System.out.println(myBinarySearchTree.remove(641));
 
-        System.out.println(myBinarySearchTree.nodeCount());
-        System.out.println(myBinarySearchTree.depth());
+//        myBinarySearchTree.reverse();
+        System.out.println(myBinarySearchTree.inorderRecursion());
 
+        System.out.println(myBinarySearchTree.preOrderRecursion());
 
-        System.out.println(myBinarySearchTree.remove(7));
-        System.out.println(myBinarySearchTree.nodeCount());
+        System.out.println(myBinarySearchTree.postOrderRecursion());
+
     }
 
 }
