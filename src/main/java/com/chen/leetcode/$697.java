5675c81dfe391d11e5932f657e939ab44776931b
sunny <2623581340@qq.com>
2020-08-19 18:29:46 +0800

    leetcode 697

---
+++ src/main/java/com/chen/leetcode/$697.java
@@ -0,0 +1,91 @@
+package com.chen.leetcode;
+
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 697. 数组的度
+ *
+ * 给定一个非空且只包含非负数的整数数组 nums, 数组的度的定义是指数组里任一元素出现频数的最大值。
+ *
+ * 你的任务是找到与 nums 拥有相同大小的度的最短连续子数组，返回其长度。
+ *
+ * 示例 1:
+ *
+ * 输入: [1, 2, 2, 3, 1]
+ * 输出: 2
+ * 解释:
+ * 输入数组的度是2，因为元素1和2的出现频数最大，均为2.
+ * 连续子数组里面拥有相同度的有如下所示:
+ * [1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
+ * 最短连续子数组[2, 2]的长度为2，所以返回2.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/degree-of-an-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * */
+public class $697 {
+
+    /**
+     * 方法：
+     * 算法：
+     *
+     * 具有度数 d 的数组必须有一些元素 x 出现 d 次。如果某些子数组具有相同的度数，那么某些元素 x （出现 d 次）。最短的子数组是将从 x 的第一次出现到最后一次出现的数组。
+     * 对于给定数组中的每个元素，让我们知道 left 是它第一次出现的索引； right 是它最后一次出现的索引。例如，当 nums = [1,2,3,2,5] 时，left[2] = 1 和 right[2] = 3。
+     * 然后，对于出现次数最多的每个元素 x，right[x] - left[x] + 1 将是我们的候选答案，我们将取这些候选的最小值。
+     *
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/degree-of-an-array/solution/shu-zu-de-du-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * 复杂度分析
+     *
+     * 时间复杂度：O(N)O(N)。其中 NN 是 nums 的长度。每个循环需要 O(N)O(N) 的时间。
+     * 空间复杂度：O(N)O(N)，left，right，count 使用的空间。
+     *
+     *
+     * */
+    public int findShortestSubArray(int[] nums) {
+
+        Map<Integer, Integer> left  = new HashMap<>();
+        Map<Integer, Integer> right = new HashMap<>();
+        Map<Integer, Integer> count = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int x = nums[i];
+            left.putIfAbsent(x, i);
+            right.put(x, i);
+            count.put(x, count.getOrDefault(x, 0) + 1);
+        }
+
+        int ans = nums.length;
+        int degree = Collections.max(count.values());
+        for (int x : count.keySet()) {
+            if (count.get(x) == degree) {
+                ans = Math.min(ans, right.get(x) - left.get(x) + 1);
+            }
+        }
+        return ans;
+    }
+
+
+    @Test
+    public void test() {
+
+        int[] nums1 = new int[5];
+        nums1[0] = 1;
+        nums1[1] = 2;
+        nums1[2] = 2;
+        nums1[3] = 3;
+        nums1[4] = 1;
+
+        System.out.println(findShortestSubArray(nums1));
+
+    }
+}
