feb5b49acda92f4f2794e3394fe91698902686fa
sunny <2623581340@qq.com>
2020-08-25 18:33:41 +0800

    leetcode 98

---
+++ src/main/java/com/chen/leetcode/tree/$98.java
@@ -0,0 +1,89 @@
+package com.chen.leetcode.tree;
+
+import org.junit.Test;
+
+/**
+ * 98. 验证二叉搜索树
+ * <p>
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ * <p>
+ * 假设一个二叉搜索树具有如下特征：
+ * <p>
+ * 节点的左子树只包含小于当前节点的数。
+ * 节点的右子树只包含大于当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * 示例 1:
+ * <p>
+ * 输入:
+ * 2
+ * / \
+ * 1   3
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:
+ * 5
+ * / \
+ * 1   4
+ *      / \
+ *     3   6
+ * 输出: false
+ * 解释: 输入为: [5,1,4,null,null,3,6]。
+ *      根节点的值为 5 ，但是其右子节点值为 4 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/validate-binary-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class $98 {
+
+    TreeNode prex;
+
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root);
+    }
+
+    /**
+     * 中序遍历，递归
+     *
+     *
+     * */
+    public boolean isValid(TreeNode node) {
+
+        if (node == null) return true;
+        if (!isValid(node.left)) return false;
+        if (prex != null && prex.val >= node.val) return false;
+        prex = node;
+        return isValid(node.right);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    '}';
+        }
+    }
+
+
+    @Test
+    public void test() {
+        TreeNode root  = new TreeNode(2);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(3);
+
+        root.left = node1;
+        root.right = node2;
+
+        System.out.println(isValidBST(root));
+    }
+}
