61fd6fb3aafbce55f73b66286bd1ce83540c6a55
sunny <2623581340@qq.com>
2020-08-27 16:53:39 +0800

    leetcode 94

---
+++ src/main/java/com/chen/leetcode/tree/$94.java
@@ -0,0 +1,102 @@
+package com.chen.leetcode.tree;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 94. 二叉树的中序遍历
+ * <p>
+ * 给定一个二叉树，返回它的中序 遍历。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [1,null,2,3]
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * <p>
+ * 输出: [1,3,2]
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class $94 {
+
+    /**
+     * 这题的真正难点在于如何用非递归的方式实现。
+     * 递归实现时，是函数自己调用自己，一层层的嵌套下去，操作系统/虚拟机自动帮我们用栈来保存了每个调用的函数，现在我们需要自己模拟这样的调用过程。
+     * 递归的调用过程是这样的：
+     *
+     * 时间复杂度:O(n)
+     * 空间复杂度:O(h)，h是树的高度
+     *
+     * 作者：wang_ni_ma
+     * 链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal/solution/dong-hua-yan-shi-94-er-cha-shu-de-zhong-xu-bian-li/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     *
+     * */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || stack.size() > 0) {
+
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                TreeNode tmp = stack.pop();
+                res.add(tmp.val);
+                root = tmp.right;
+            }
+        }
+        return res;
+    }
+
+
+
+
+    @Test
+    public void test() {
+        TreeNode root = new TreeNode(9);
+
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node0 = new TreeNode(0);
+        TreeNode node8 = new TreeNode(8);
+
+
+        TreeNode node10 = new TreeNode(10);
+
+
+
+        root.left = node3;
+
+        node3.left = node5;
+        node3.right = node1;
+
+        node5.left = node6;
+        node5.right = node2;
+
+        node2.left = node7;
+        node2.right = node4;
+
+        node1.left = node0;
+        node1.right = node8;
+
+        System.out.println(inorderTraversal(root));
+    }
+}
