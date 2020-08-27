4b91aad899d4d902d9d4c7003edf290ff44af66b
sunny <2623581340@qq.com>
2020-08-27 17:19:24 +0800

    leetcode 144

---
+++ src/main/java/com/chen/leetcode/tree/$144.java
@@ -0,0 +1,87 @@
+package com.chen.leetcode.tree;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ *
+ * 144. 二叉树的前序遍历
+ *
+ * 给定一个二叉树，返回它的 前序 遍历。
+ *
+ *  示例:
+ *
+ * 输入: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * 输出: [1,2,3]
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-preorder-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * */
+public class $144 {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || stack.size() > 0) {
+
+            if (root != null) {
+                res.add(root.val);
+                stack.push(root.right);
+                root = root.left;
+            } else {
+                TreeNode tmp = stack.pop();
+                root = tmp;
+            }
+        }
+        return res;
+    }
+
+    @Test
+    public void test() {
+        TreeNode root = new TreeNode(9);
+
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node0 = new TreeNode(0);
+        TreeNode node8 = new TreeNode(8);
+
+
+        TreeNode node10 = new TreeNode(10);
+
+
+
+        root.left = node3;
+
+        node3.left = node5;
+        node3.right = node1;
+
+        node5.left = node6;
+        node5.right = node2;
+
+        node2.left = node7;
+        node2.right = node4;
+
+        node1.left = node0;
+        node1.right = node8;
+
+        System.out.println(preorderTraversal(root));
+    }
+
+}
