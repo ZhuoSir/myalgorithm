fa64a37d6c6d855cde75196c25601d93b8efcd8a
sunny <2623581340@qq.com>
2020-08-27 12:10:06 +0800

    leetcode 543

---
+++ src/main/java/com/chen/leetcode/tree/$543.java
@@ -0,0 +1,70 @@
+package com.chen.leetcode.tree;
+
+import org.junit.Test;
+
+/**
+ * 543. 二叉树的直径
+ *
+ * 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
+ *
+ *  
+ *
+ * 示例 :
+ * 给定二叉树
+ *
+ *           1
+ *          / \
+ *         2   3
+ *        / \
+ *       4   5
+ * 返回 3, 它的长度是路径 [4,2,1,3] 或者 [5,2,1,3]。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/diameter-of-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * */
+public class $543 {
+
+    int max = 0;
+
+    /**
+     *
+     * 复杂度分析
+     *
+     * 时间复杂度：O(N)O(N)，其中 NN 为二叉树的节点数，即遍历一棵二叉树的时间复杂度，每个结点只被访问一次。
+     *
+     * 空间复杂度：O(Height)O(Height)，其中 HeightHeight 为二叉树的高度。由于递归函数在递归过程中需要为每一层递归函数分配栈空间，所以这里需要额外的空间且该空间取决于递归的深度，而递归的深度显然为二叉树的高度，并且每次递归调用的函数里又只用了常数个变量，所以所需空间复杂度为 O(Height)O(Height) 。
+     *
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/diameter-of-binary-tree/solution/er-cha-shu-de-zhi-jing-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * */
+    public int diameterOfBinaryTree(TreeNode root) {
+
+        if (root == null)
+            return 0;
+
+        depth(root);
+        return max - 1;
+    }
+
+    private int depth(TreeNode node) {
+
+        if (node == null) return 0;
+        int ldepth = depth(node.left);
+        int rdepth = depth(node.right);
+
+        max = Math.max(ldepth + rdepth + 1, max);
+        return Math.max(ldepth, rdepth) + 1;
+    }
+
+
+    @Test
+    public void test() {
+
+
+    }
+}
