6d261547f6bfc0b56c39093fabe4f11224ea692a
sunny <2623581340@qq.com>
2020-09-09 17:20:42 +0800

    leetcode 107

---
+++ src/main/java/com/chen/leetcode/bfs/$107.java
@@ -0,0 +1,65 @@
+package com.chen.leetcode.bfs;
+
+import com.chen.leetcode.tree.TreeNode;
+
+import java.util.*;
+
+/**
+ * 107. 二叉树的层次遍历 II
+ *
+ * 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ *
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回其自底向上的层次遍历为：
+ *
+ * [
+ *   [15,7],
+ *   [9,20],
+ *   [3]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * */
+public class $107 {
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+        if (root == null)
+            return new ArrayList<>();
+
+        List<List<Integer>> ret = new LinkedList<>();
+
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+
+            ret.add(0, level);
+        }
+
+        return ret;
+    }
+
+}
