6a1a07be8c5714a861bc9e8454b1e87ea6869f1e
sunny <2623581340@qq.com>
2020-09-11 11:30:02 +0800

    leetcode 101

---
+++ src/main/java/com/chen/leetcode/tree/$101.java
@@ -0,0 +1,125 @@
+package com.chen.leetcode.tree;
+
+import org.junit.Test;
+
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 101
+ *
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ *  
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ *  
+ *
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/symmetric-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ *
+ * */
+public class $101 {
+
+    /**
+     * 递归遍历
+     *
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(N)
+     *
+     * */
+    public boolean isSymmetric(TreeNode root) {
+
+        if (root == null)
+            return true;
+
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+
+        if (left == null && right == null)
+            return true;
+
+        if (left == null || right == null)
+            return false;
+
+        return left.val == right.val
+                && isSymmetric(left.left, right.right)
+                && isSymmetric(left.right, right.left);
+    }
+
+    /**
+     * 用队列代替递归，广度优先；
+     *
+     * */
+    public boolean isSymmetric2(TreeNode root) {
+
+        if (root == null)
+            return true;
+
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+
+        while (!queue.isEmpty()) {
+            TreeNode l = queue.poll();
+            TreeNode r = queue.poll();
+
+            if (l == null && r == null)
+                continue;
+
+            if ((l == null || r == null) || (l.val != r.val)) {
+                return false;
+            }
+
+            queue.add(l.left);
+            queue.add(r.right);
+
+            queue.add(l.right);
+            queue.add(r.left);
+        }
+
+        return true;
+    }
+
+    @Test
+    public void test() {
+
+        TreeNode treeNode = new TreeNode(1);
+        TreeNode treeNode1 = new TreeNode(2);
+        TreeNode treeNode2 = new TreeNode(2);
+        TreeNode treeNode3 = new TreeNode(3);
+        TreeNode treeNode4 = new TreeNode(3);
+        TreeNode treeNode5 = new TreeNode(4);
+        TreeNode treeNode6 = new TreeNode(4);
+
+        treeNode.left = treeNode1;
+        treeNode.right = treeNode2;
+
+        treeNode1.left = treeNode3;
+        treeNode1.right = treeNode5;
+
+        treeNode2.left = treeNode6;
+        treeNode2.right = treeNode4;
+
+        System.out.println(isSymmetric(treeNode));
+    }
+}
