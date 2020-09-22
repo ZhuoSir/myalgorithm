5f791679ce3a1b3c8a63a4281cfd580c5025c7b8
sunny <2623581340@qq.com>
2020-09-22 14:05:02 +0800

    leetcode 58

---
+++ src/main/java/com/chen/leetcode/string/$58.java
@@ -0,0 +1,32 @@
+package com.chen.leetcode.string;
+
+import org.junit.Test;
+
+/**
+ * 58. 最后一个单词的长度
+ * <p>
+ * 给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。
+ * <p>
+ * 如果不存在最后一个单词，请返回 0 。
+ * <p>
+ * 说明：一个单词是指仅由字母组成、不包含任何空格字符的 最大子字符串。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/length-of-last-word
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class $58 {
+
+    public int lengthOfLastWord(String s) {
+        if (s == null || s.isEmpty()) return 0;
+        String[] array = s.split(" ");
+        return array.length > 0 ? array[array.length - 1].length() : 0;
+    }
+
+    @Test
+    public void test() {
+
+        System.out.println(lengthOfLastWord("a"));
+
+    }
+}
