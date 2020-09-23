09c15cd4e57665def34edfb282ad9c592e641b64
sunny <2623581340@qq.com>
2020-09-23 10:12:37 +0800

    leetcode 102 深度优先实现

--- src/main/java/com/chen/leetcode/tree/$102.java
+++ src/main/java/com/chen/leetcode/tree/$102.java
@@ -10,44 +10,41 @@ import java.util.Queue;
 
 /**
  * 102. 二叉树的层序遍历
- *
+ * <p>
  * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
- *
+ * <p>
  *  
- *
+ * <p>
  * 示例：
  * 二叉树：[3,9,20,null,null,15,7],
- *
- *     3
- *    / \
- *   9  20
- *     /  \
- *    15   7
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
  * 返回其层次遍历结果：
- *
+ * <p>
  * [
- *   [3],
- *   [9,20],
- *   [15,7]
+ * [3],
+ * [9,20],
+ * [15,7]
  * ]
- *
- *
+ * <p>
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
- *
- * */
+ */
 public class $102 {
 
     /**
      * BFS广度优先搜索，外加分层；
-     *
-     *
-     * */
+     */
     public List<List<Integer>> levelOrder(TreeNode root) {
 
         if (root == null)
-            return new ArrayList<>();;
+            return new ArrayList<>();
 
         List<List<Integer>> ret = new ArrayList<>();
         Queue<TreeNode> queue = new ArrayDeque<>();
@@ -74,8 +71,36 @@ public class $102 {
 
     public List<List<Integer>> levelOrder2(TreeNode root) {
 
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        if (root.left == null && root.right == null) {
+            List<Integer> r = new ArrayList<>();
+            r.add(root.val);
+            res.add(r);
+            return res;
+        }
 
-        return null;
+        int depth = depth(root);
+        for (int i = 0; i < depth; i++) {
+            res.add(new ArrayList<>());
+        }
+        dfs(0, root, res);
+        return res;
+    }
+
+
+    private int depth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(depth(root.left), depth(root.right));
+    }
+
+    private void dfs(int level, TreeNode node, List<List<Integer>> res) {
+        if (node == null)
+            return;
+        List<Integer> r = res.get(level);
+        r.add(node.val);
+        dfs(level + 1, node.left, res);
+        dfs(level + 1, node.right, res);
     }
 
 
@@ -93,6 +118,6 @@ public class $102 {
         node2.left = node3;
         node2.right = node4;
 
-        System.out.println(levelOrder(root));
+        System.out.println(levelOrder2(null));
     }
 }
