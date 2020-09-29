6cd67d70c1998eff3cde912e40828ab482876ea9
sunny <2623581340@qq.com>
2020-09-29 20:12:49 +0800

    leetcode 645

---
+++ src/main/java/com/chen/leetcode/array/$645.java
@@ -0,0 +1,48 @@
+package com.chen.leetcode.array;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 集合 S 包含从1到 n 的整数。不幸的是，因为数据错误，导致集合里面某一个元素复制了成了集合里面的另外一个元素的值，导致集合丢失了一个整数并且有一个元素重复。
+ * <p>
+ * 给定一个数组 nums 代表了集合 S 发生错误后的结果。你的任务是首先寻找到重复出现的整数，再找到丢失的整数，将它们以数组的形式返回。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [1,2,2,4]
+ * 输出: [2,3]
+ * 注意:
+ * <p>
+ * 给定数组的长度范围是 [2, 10000]。
+ * 给定的数组是无序的。
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/set-mismatch
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class $645 {
+
+    public int[] findErrorNums(int[] nums) {
+        Arrays.sort(nums);
+        int dup = -1, missing = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1])
+                dup = nums[i];
+            else if (nums[i] > nums[i - 1] + 1)
+                missing = nums[i - 1] + 1;
+        }
+        return new int[] {dup, nums[nums.length - 1] != nums.length ? nums.length : missing};
+    }
+
+    @Test
+    public void test() {
+        int[] res = findErrorNums(new int[]{1,1});
+        System.out.println(res[0]);
+        System.out.println(res[1]);
+    }
+}
