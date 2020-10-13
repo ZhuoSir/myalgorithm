9c7cdbe074673c934b2585d4f6a8b9de997e6494
sunny <2623581340@qq.com>
2020-10-13 10:27:12 +0800

    leetcode 199 增加dfs 方式

--- src/main/java/com/chen/leetcode/tree/$199.java
+++ src/main/java/com/chen/leetcode/tree/$199.java
@@ -7,34 +7,30 @@ import java.util.*;
 
 /**
  * 199. 二叉树的右视图
- *
+ * <p>
  * 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
- *
+ * <p>
  * 示例:
- *
+ * <p>
  * 输入: [1,2,3,null,5,null,4]
  * 输出: [1, 3, 4]
  * 解释:
- *
- *    1            <---
- *  /   \
+ * <p>
+ * 1            <---
+ * /   \
  * 2     3         <---
- *  \     \
- *   5     4       <---
- *
+ * \     \
+ * 5     4       <---
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/binary-tree-right-side-view
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
- *
- *
- * */
+ */
 public class $199 {
 
     /**
      *
-     *
-     *
-     * */
+     */
     public List<Integer> rightSideView(TreeNode root) {
 
         if (root == null)
@@ -73,6 +69,23 @@ public class $199 {
         return res;
     }
 
+
+    public List<Integer> rightSideView2(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
+        dfs(0, root, res);
+        return res;
+    }
+
+    private void dfs(int level, TreeNode node, List<Integer> res) {
+
+        if (node == null) return;
+        if (res.size() == level) res.add(node.val);
+
+        dfs(level + 1, node.right, res);
+        dfs(level + 1, node.left, res);
+    }
+
     @Test
     public void test() {
 
@@ -80,14 +93,16 @@ public class $199 {
         TreeNode node2 = new TreeNode(2);
         TreeNode node3 = new TreeNode(3);
         TreeNode node4 = new TreeNode(4);
-        TreeNode node5 = new TreeNode(5);
+//        TreeNode node5 = new TreeNode(5);
 
         node1.left = node2;
         node1.right = node3;
 
-        node2.right = node5;
-        node3.right = node4;
+        node2.left = node4;
+//
+//        node2.right = node5;
+//        node3.right = node4;
 
-        System.out.println(rightSideView(node1));
+        System.out.println(rightSideView2(node1));
     }
 }
