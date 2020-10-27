6a3a43992316c74f1571472fd2292421bf12eb8b
chenzhuo <chenzhuo@yuntongxun.com>
2020-10-27 14:42:35 +0800

    leetcode 129

---
+++ src/main/java/com/chen/leetcode/tree/$129.java
@@ -0,0 +1,82 @@
+package com.chen.leetcode.tree;
+
+import com.chen.leetcode.common.TreeNode;
+import org.junit.Test;
+
+/**
+ * 给定一个二叉树，它的每个结点都存放一个 0-9 的数字，每条从根到叶子节点的路径都代表一个数字。
+ * <p>
+ * 例如，从根到叶子节点路径 1->2->3 代表数字 123。
+ * <p>
+ * 计算从根到叶子节点生成的所有数字之和。
+ * <p>
+ * 说明: 叶子节点是指没有子节点的节点。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,2,3]
+ * 1
+ * / \
+ * 2   3
+ * 输出: 25
+ * 解释:
+ * 从根到叶子节点路径 1->2 代表数字 12.
+ * 从根到叶子节点路径 1->3 代表数字 13.
+ * 因此，数字总和 = 12 + 13 = 25.
+ * 示例 2:
+ * <p>
+ * 输入: [4,9,0,5,1]
+ * 4
+ * / \
+ * 9   0
+ *  / \
+ * 5   1
+ * 输出: 1026
+ * 解释:
+ * 从根到叶子节点路径 4->9->5 代表数字 495.
+ * 从根到叶子节点路径 4->9->1 代表数字 491.
+ * 从根到叶子节点路径 4->0 代表数字 40.
+ * 因此，数字总和 = 495 + 491 + 40 = 1026.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sum-root-to-leaf-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class $129 {
+
+    int res = 0;
+
+    public int sumNumbers(TreeNode root) {
+        dfs(0, root);
+        return res;
+    }
+
+    private void dfs(int r, TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        int nr = r * 10 + node.val;
+        if (node.left == null && node.right == null) {
+            res += nr;
+            return;
+        }
+        dfs(nr, node.left);
+        dfs(nr, node.right);
+    }
+
+    @Test
+    public void test() {
+        TreeNode node1 = new TreeNode(4);
+        TreeNode node2 = new TreeNode(9);
+        TreeNode node3 = new TreeNode(0);
+        TreeNode node4 = new TreeNode(5);
+        TreeNode node5 = new TreeNode(1);
+
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+
+        System.out.println(sumNumbers(node1));
+    }
+}
