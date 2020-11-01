d53045b4e32f90ab9063742a2c8dac11dc2d01c8
sunny <2623581340@qq.com>
2020-11-01 23:15:28 +0800

    leetcode 140 待优化

--- src/main/java/com/chen/leetcode/bp/$140.java
+++ src/main/java/com/chen/leetcode/bp/$140.java
@@ -2,9 +2,7 @@ package com.chen.leetcode.bp;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，在字符串中增加空格来构建一个句子，使得句子中所有的单词都在词典中。返回所有这些可能的句子。
@@ -51,42 +49,127 @@ public class $140 {
 
     List<List<String>> strs = new ArrayList<>();
 
+    Set<String> wordSet = null;
+
     public List<String> wordBreak(String s, List<String> wordDict) {
         List<String> res = new ArrayList<>();
-        strs = dfs(new ArrayList<>(), s, wordDict, 0);
+        wordSet = new HashSet<>(wordDict);
+        dfs(new ArrayList<>(), s, 0);
         if (strs == null) return null;
         for (int i = 0; i < strs.size(); i++) {
             StringBuilder str = new StringBuilder();
-            for (int j = 0; j < strs.get(i).size(); j++) {
-                str.append(strs.get(j)).append(" ");
+            List<String> list = strs.get(i);
+            for (int j = 0; j < list.size(); j++) {
+                str.append(list.get(j));
+                if (j != list.size() - 1) {
+                    str.append(" ");
+                }
             }
             res.add(str.toString());
         }
         return res;
     }
 
-    private List<List<String>> dfs(List<String> strs, String word, List<String> wordDict, int start) {
+    private void dfs(List<String> list, String word, int start) {
         if (start == word.length()) {
-            return null;
+            strs.add(new ArrayList<>(list));
+            return;
         }
 
         StringBuilder tmp = new StringBuilder();
         for (int i = start; i < word.length(); i++) {
             tmp.append(word.charAt(i));
-            if (wordDict.contains(tmp.toString())) {
-                strs.add(tmp.toString());
-                dfs(strs, word, wordDict, i + 1);
-                strs.remove(strs.size() - 1);
+            if (wordSet.contains(tmp.toString())) {
+                list.add(tmp.toString());
+                dfs(list, word, i + 1);
+                list.remove(list.size() - 1);
             }
         }
 
-        return null;
+        return;
+    }
+
+
+    public List<String> wordBreak2(String s, List<String> wordDict) {
+        // 为了快速判断一个单词是否在单词集合中，需要将它们加入哈希表
+        Set<String> wordSet = new HashSet<>(wordDict);
+        int len = s.length();
+
+        // 第 1 步：动态规划计算是否有解
+        // dp[i] 表示「长度」为 i 的 s 前缀子串可以拆分成 wordDict 中的单词
+        // 长度包括 0 ，因此状态数组的长度为 len + 1
+        boolean[] dp = new boolean[len + 1];
+        // 0 这个值需要被后面的状态值参考，如果一个单词正好在 wordDict 中，dp[0] 设置成 true 是合理的
+        dp[0] = true;
+
+        for (int right = 1; right <= len; right++) {
+            // 如果单词集合中的单词长度都不长，从后向前遍历是更快的
+            for (int left = right - 1; left >= 0; left--) {
+                // substring 不截取 s[right]，dp[left] 的结果不包含 s[left]
+                if (wordSet.contains(s.substring(left, right)) && dp[left]) {
+                    dp[right] = true;
+                    // 这个 break 很重要，一旦得到 dp[right] = True ，不必再计算下去
+                    break;
+                }
+            }
+        }
+
+        // 第 2 步：回溯算法搜索所有符合条件的解
+        List<String> res = new ArrayList<>();
+        if (dp[len]) {
+            Deque<String> path = new ArrayDeque<>();
+            dfs2(s, len, wordSet, dp, path, res);
+            return res;
+        }
+        return res;
+    }
+
+    /**
+     * s[0:len) 如果可以拆分成 wordSet 中的单词，把递归求解的结果加入 res 中
+     *
+     * @param s
+     * @param len     长度为 len 的 s 的前缀子串
+     * @param wordSet 单词集合，已经加入哈希表
+     * @param dp      预处理得到的 dp 数组
+     * @param path    从叶子结点到根结点的路径
+     * @param res     保存所有结果的变量
+     */
+    private void dfs2(String s, int len, Set<String> wordSet, boolean[] dp, Deque<String> path, List<String> res) {
+        if (len == 0) {
+            res.add(String.join(" ",path));
+            return;
+        }
+
+        // 可以拆分的左边界从 len - 1 依次枚举到 0
+        for (int i = len - 1; i >= 0; i--) {
+            String suffix = s.substring(i, len);
+            if (wordSet.contains(suffix) && dp[i]) {
+                path.addFirst(suffix);
+                dfs2(s, i, wordSet, dp, path, res);
+                path.removeFirst();
+            }
+        }
     }
 
     @Test
     public void test() {
-        String word = "";
+        String word = "catsandog";
         List<String> dict = Arrays.asList("cat", "cats", "and", "sand", "dog");
         System.out.println(wordBreak(word, dict));
+    }
+
+    @Test
+    public void test2() {
+        String word = "pineapplepenapple";
+        List<String> dict = Arrays.asList("apple", "pen", "applepen", "pine", "pineapple");
+        System.out.println(wordBreak(word, dict));
+    }
+
+    @Test
+    public void test3() {
+
+        String word = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        List<String> dict = Arrays.asList("a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa");
+        System.out.println(wordBreak2(word, dict));
     }
 }
