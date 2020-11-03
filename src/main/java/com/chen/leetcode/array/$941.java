8bdbd0d5b67d38c89ec6979263e555b4e3b8086c
卓sir <603334965@qq.com>
2020-11-03 19:43:28 +0800

    leetcode 941

---
+++ src/main/java/com/chen/leetcode/array/$941.java
@@ -0,0 +1,67 @@
+package com.chen.leetcode.array;
+
+import org.junit.Test;
+
+/**
+ * 给定一个整数数组 A，如果它是有效的山脉数组就返回 true，否则返回 false。
+ * <p>
+ * 让我们回顾一下，如果 A 满足下述条件，那么它是一个山脉数组：
+ * <p>
+ * A.length >= 3
+ * 在 0 < i < A.length - 1 条件下，存在 i 使得：
+ * A[0] < A[1] < ... A[i-1] < A[i]
+ * A[i] > A[i+1] > ... > A[A.length - 1]
+ *  
+ * <p>
+ * <p>
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：[2,1]
+ * 输出：false
+ * 示例 2：
+ * <p>
+ * 输入：[3,5,5]
+ * 输出：false
+ * 示例 3：
+ * <p>
+ * 输入：[0,3,2,1]
+ * 输出：true
+ *  
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-mountain-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class $941 {
+
+    public boolean validMountainArray(int[] A) {
+        int N = A.length;
+        int i = 0;
+
+        // 递增扫描
+        while (i + 1 < N && A[i] < A[i + 1]) {
+            i++;
+        }
+
+        // 最高点不能是数组的第一个位置或最后一个位置
+        if (i == 0 || i == N - 1) {
+            return false;
+        }
+
+        // 递减扫描
+        while (i + 1 < N && A[i] > A[i + 1]) {
+            i++;
+        }
+
+        return i == N - 1;
+    }
+
+    @Test
+    public void test() {
+        int[] array = new int[]{3, 5, 5};
+        System.out.println(validMountainArray(array));
+    }
+}
